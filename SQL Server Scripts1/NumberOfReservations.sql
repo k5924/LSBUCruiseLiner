295a5f1d18341cd90bd50f716a9938b7938e70a2
Kamran <56407135+k5924@users.noreply.github.com>
2021-04-26 09:50:50 +0100

    Fixed: 4.1 by creating a view

--- SQL Server Scripts1/NumberOfReservations.sql
+++ SQL Server Scripts1/NumberOfReservations.sql
@@ -1,11 +1,24 @@
 USE Cruise_Liner;
 GO
 
-SELECT FirstName, LastName, OfficeAddress, OfficeZipcode, NumberOfReservations
-FROM(
+-- for seeing only the highest and lowest reservations by employee
+DROP VIEW IF EXISTS query1;
+GO
+
+CREATE VIEW query1 as(
+SELECT FirstName, LastName, OfficeAddress, OfficeZipcode, COUNT(EmployeesID) as NumberOfReservations
+FROM Employees, Reservation
+WHERE Employees.ID = Reservation.EmployeesID
+GROUP BY FirstName, LastName, OfficeAddress, OfficeZipcode);
+GO
+
+SELECT * 
+FROM query1
+WHERE query1.NumberOfReservations=(SELECT MIN(NumberOfReservations) FROM query1) OR query1.NumberOfReservations=(SELECT MAX(NumberOfReservations) FROM query1);
+
+-- to see all employees ordered by number of reservations
 SELECT FirstName, LastName, OfficeAddress, OfficeZipcode, COUNT(EmployeesID) as NumberOfReservations
 FROM Employees, Reservation
 WHERE Employees.ID = Reservation.EmployeesID
 GROUP BY FirstName, LastName, OfficeAddress, OfficeZipcode
-) a
-WHERE NumberOfReservations=(SELECT MIN(COUNT(EmployeesID)) FROM Reservation);
+ORDER BY NumberOfReservations DESC;
